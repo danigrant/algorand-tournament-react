533d5d2b24630058845550ba45b03c25cac49b6e
dani <mayor@dani.town>
2019-11-18 10:17:00 -0500

    init

---
+++ pages/Leaderboard.js
@@ -0,0 +1,25 @@
+import AppContainer from '../components/AppContainer'
+import Header from '../components/Header'
+
+const Leaderboard = props => {
+  return (
+    <div>
+      <Header />
+      <AppContainer>
+        <p className="margin-top-lrg font-color-yellow font-weight-bold">Leaderboard</p>
+        <p><span className="font-weight-bold font-color-green">114:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">165:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">390:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">579:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">627:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">633:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">732:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">906:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">921:</span> Still unsolved, could be you!</p>
+        <p><span className="font-weight-bold font-color-green">975:</span> Still unsolved, could be you!</p>
+      </AppContainer>
+    </div>
+  )
+}
+
+export default Leaderboard
